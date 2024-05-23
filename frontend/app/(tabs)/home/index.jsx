870688d4df3e2d1024a01dc2eab1d137e62b205f
vaninaNina <kalapsazova.vanina@gmail.com>
2024-05-23 20:10:47 +0300

    feature/home-screen
    home screen with hardcoded data together with the header component

--- frontend/app/(tabs)/home/index.jsx
+++ frontend/app/(tabs)/home/index.jsx
@@ -1,20 +1,64 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { styles } from "./homeStyles.js";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ImageBackground,
+  ScrollView,
+  Pressable,
+} from "react-native";
+
+import { Header } from "../../components/header.jsx";
+
+import KITCHEN_SOURCE from "../../../assets/kitchen-pic.jpg";
+import BATHROOM_SOURCE from "../../../assets/bathroom.jpg";
+import TOILET_SOURCE from "../../../assets/toilet.png";
 
 const Home = () => {
   return (
-    <View style={styles.container}>
-      <Text>Welcome to the Home Screen!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView
+        contentContainerStyle={styles.scrollViewContent}
+        showsVerticalScrollIndicator={false}
+      >
+        <Header showProfilePic={true} />
+        <View style={styles.text}>
+          <Text style={styles.headerTitle}>Здравей, username!</Text>
+          <Text style={styles.description}>
+            За коя част от дома искаш да провериш потреблението си?
+          </Text>
+        </View>
+        <Pressable
+          style={styles.paddingZero}
+          onPress={() => console.log("TODO: redirect")}
+        >
+          <ImageBackground style={styles.rooms} source={KITCHEN_SOURCE}>
+            <Text style={styles.roomText}>Кухня</Text>
+            <Text style={styles.devices}>5 уреда</Text>
+          </ImageBackground>
+        </Pressable>
+        <Pressable
+          style={styles.paddingZero}
+          onPress={() => console.log("TODO: redirect")}
+        >
+          <ImageBackground style={styles.rooms} source={BATHROOM_SOURCE}>
+            <Text style={styles.roomText}>Баня</Text>
+            <Text style={styles.devices}>4 уреда</Text>
+          </ImageBackground>
+        </Pressable>
+        <Pressable
+          style={styles.paddingZero}
+          onPress={() => console.log("TODO: redirect")}
+        >
+          <ImageBackground style={styles.rooms} source={TOILET_SOURCE}>
+            <Text style={styles.roomText}>Тоалетна</Text>
+            <Text style={styles.devices}>2 уреда</Text>
+          </ImageBackground>
+        </Pressable>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
 
 export default Home;
---
+++ frontend/app/components/headerStyles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  pics: {
+    width: 55,
+    height: 55,
+    borderRadius: 50,
+  },
+  icon: {
+    paddingTop: 10,
+  },
+});
