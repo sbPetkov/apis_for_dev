efcecefe4ac1d41831676ab9cd0aa4840ea097f4
sbPetkov <svilen.petkov@icloud.com>
2024-05-29 11:44:35 +0300

    Added CORS cross origins
    Fixing the issue with other ports having no access to the server

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -7,7 +7,30 @@ SECRET_KEY = 'django-insecure-(k5m(@%is@wac$)!gt&_m%d$uro+2*w&ote24fzj)jdm9$uxuk
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1',]
+
+CORS_ALLOW_ALL_ORIGINS = True  # For development only
+# CORS_ALLOWED_ORIGINS = [
+#     'http://127.0.0.1:8081',
+#     'http://localhost:3000',
+#     'http://192.168.1.100:8081',  # Example local network IP
+#     'http://yourfrontenddomain.com',
+# ]
+
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_METHODS = [
+    "GET",
+    "POST",
+    "PUT",
+    "PATCH",
+    "DELETE",
+    "OPTIONS",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'http://127.0.0.1:8081',
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -17,18 +40,19 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'corsheaders',
 
     'api.profiles_api',
     'api.advices.apps.AdvicesConfig',
     'rest_framework',
     'rest_framework.authtoken',
 
-
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -86,7 +110,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
